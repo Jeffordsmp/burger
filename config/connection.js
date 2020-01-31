548f4141b33ef254f8f50edff0173b4055a50d69
Choop <matthewjeffords97@yahoo.com>
2020-01-31 14:35:31 -0700

    adding jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Bball123",
-  database: "burgers_db"
-});
+let connection;
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Bball123',
+    database: 'burgers_db'
   }
-  console.log("connected as id " + connection.threadId);
-});
+}
 
+connection.connect()
 module.exports = connection;
